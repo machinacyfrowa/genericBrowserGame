81d6ed42fe17f1807e707f25aab850e53399fb20
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2021-03-23 12:57:42 +0100

    wprowadzenie do smarty

---
+++ smarty/templates_c/3e392a0b2c95d4a3e4571f91d3bf93552586a058_0.file.header.tpl.php
@@ -0,0 +1,34 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-03-23 12:51:04
+  from 'C:\xampp\htdocs\genericBrowserGame\smarty\templates\header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6059d628989b17_15948357',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '3e392a0b2c95d4a3e4571f91d3bf93552586a058' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\genericBrowserGame\\smarty\\templates\\header.tpl',
+      1 => 1616500231,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6059d628989b17_15948357 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo (($tmp = @$_smarty_tpl->tpl_vars['title']->value)===null||$tmp==='' ? "Strategiczna gra przeglądarkowa" : $tmp);?>
+</title>
+</head>
+<body><?php }
+}
